81f860f787cb2265d90206115d40075bd8c1ba45
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-28 11:46:59 +0200

    Faker

---
+++ Shopper.Infrastructure/Fakers/ProductFaker.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Shopper.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopper.Infrastructure.Fakers
+{
+    // dotnet add package Bogus
+    public class ProductFaker : Faker<Product>
+    {
+
+    }
+}
