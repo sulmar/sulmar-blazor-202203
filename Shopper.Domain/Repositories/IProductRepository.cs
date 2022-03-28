ccd2a03ae4aed8693336612d736590b031d4a93e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-28 11:41:44 +0200

    Dodanie repozytorium

---
+++ Shopper.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shopper.Domain.Models;
+
+namespace Shopper.Domain.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAsync();
+    Task<Product> GetByIdAsync(int id);
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task RemoveAsync(int id);
+    Task<bool> ExistsAsync(int id);
+}
+
