dfe05f7bc20885857aa7d6f79a68cc436e219c32
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-29 13:34:08 +0200

    Dodanie OpenApi (swagger)

--- Shopper.Api/Program.cs
+++ Shopper.Api/Program.cs
@@ -1,14 +1,23 @@
 using Bogus;
+using Microsoft.AspNetCore.Http.Json;
 using Shopper.Domain.Models;
 using Shopper.Domain.Repositories;
 using Shopper.Infrastructure;
 using Shopper.Infrastructure.Fakers;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IProductRepository, FakeProductRepository>();
 builder.Services.AddSingleton<Faker<Product>, ProductFaker>();
 
+builder.Services.AddSingleton<ICustomerRepository, FakeCustomerRepository>();
+builder.Services.AddSingleton<Faker<Customer>, CustomerFaker>();
+
+builder.Services.AddEndpointsApiExplorer();
+
+// Install-Package Swashbuckle.AspNetCore - Version 6.2.3
+builder.Services.AddSwaggerGen();
 
 //builder.Services.AddCors(policy =>
 //{
@@ -17,6 +26,9 @@ builder.Services.AddSingleton<Faker<Product>, ProductFaker>();
 //        .AllowAnyOrigin()            
 //        .AllowAnyMethod()
 //        .AllowAnyHeader());
+
+builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 
 builder.Services.AddCors(
     policy => policy.AddDefaultPolicy(
@@ -39,5 +51,20 @@ app.MapGet("api/products", async
 // GET api/products/{id}
 app.MapGet("api/products/{id:int}", async
     (IProductRepository productRepository, int id) => await productRepository.GetByIdAsync(id));
+
+// GET api/customers
+app.MapGet("api/customers", async (ICustomerRepository customerRepository) => await customerRepository.GetAsync());
+
+// GET api/customers/{id}
+app.MapGet("api/customers/{id:int}", async (ICustomerRepository customerRepository, int id) => await customerRepository.GetByIdAsync(id));
+
+// GET api/customers/search?name="John"
+app.MapGet("api/customers/search", async (ICustomerRepository customerRepository, string name) => await customerRepository.GetByName(name));
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
