168200d3530d92b27bf0e761660fdf2f52e4bc91
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-31 16:15:48 +0200

    Utworzenie listy klientów

---
+++ Shopper.BlazorWebAssembly/Pages/Customers/CustomerList.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shopper.Domain.Models;
+using Shopper.Domain.Services;
+
+namespace Shopper.BlazorWebAssembly.Pages.Customers
+{
+    public partial class CustomerList 
+    {
+        private IEnumerable<Customer> customers;
+
+        [Inject]
+        public ICustomerService CustomerService { get; set; }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            customers = await CustomerService.GetAsync();
+
+            StateHasChanged();
+        }
+
+    }
+}
