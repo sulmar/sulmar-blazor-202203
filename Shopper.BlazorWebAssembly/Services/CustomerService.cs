f332cb53d30052d5e7c341490e28c76db743d3e2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-31 16:20:12 +0200

    Dodanie CustomerService

---
+++ Shopper.BlazorWebAssembly/Services/CustomerService.cs
@@ -0,0 +1,41 @@
+﻿using Shopper.Domain.Models;
+using Shopper.Domain.Services;
+using System.Net.Http.Json;
+
+namespace Shopper.BlazorWebAssembly.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly HttpClient client;
+
+        public CustomerService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAsync(CancellationToken token = default)
+        {
+            return await client.GetFromJsonAsync<IEnumerable<Customer>>("api/customers", token);
+        }
+
+        public Task<Customer> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
