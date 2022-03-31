31ba0f3629ac81c8b0c5076e91d537dfd77419eb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-31 14:33:03 +0200

    Przechowywanie licznika w LocalStorage

--- Shopper.BlazorWebAssembly/Program.cs
+++ Shopper.BlazorWebAssembly/Program.cs
@@ -2,6 +2,7 @@ using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
 using Shopper.BlazorWebAssembly;
 using Shopper.BlazorWebAssembly.Services;
 using Shopper.Domain.Services;
@@ -37,5 +38,9 @@ builder.Services.AddSingleton<HubConnection>(options =>
 
 // dotnet add package Blazored.Toast
 builder.Services.AddBlazoredToast();
+
+
+builder.Services
+    .AddSingleton<IJSInProcessRuntime>(sp=>(IJSInProcessRuntime)sp.GetRequiredService<IJSRuntime>());
 
 await builder.Build().RunAsync();
