c24f65d5096300b6bad0ba610eefc58902a29758
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-04-05 11:12:46 +0200

    Dodanie przykładu testu

---
+++ Shopper.BlazorServerSide.Tests/CounterTests.cs
@@ -0,0 +1,24 @@
+﻿using Bunit;
+using Shopper.BlazorServerSide.Pages;
+using Xunit;
+
+namespace Shopper.Tests
+{
+    public class CounterTests : TestContext
+    {
+        [Fact]
+        public void Counter_WhenClicked_ShouldIncrement()
+        {
+            // Arrange
+            IRenderedFragment cut = RenderComponent<Counter>();
+
+            // Act
+            var button = cut.Find("button");
+            button.Click();
+
+            // Assert
+            var paragraph = cut.Find("p");
+            paragraph.MarkupMatches(@"<p role=""status"">Current count: 1</p>");
+        }
+    }
+}
